2a8193507a6556972c8da6c8ff60ffde1c350cdf
Shivansh Rai <shivansh@freebsd.org>
2016-01-23 10:34:00 +0500

    Improve precision: Exponential notation upto 6 digits after the decimal

--- prob5/prob5.m
+++ prob5/prob5.m
@@ -1,10 +1,12 @@
-n = 0;
+n = 11;
 fp = fopen('prob5/error-values', 'w');
-while n < 100
-    % Avoid calculating binCoeff twice when inequality occurs
+% Avoid calculating binCoeff twice when inequality occurs
+% Precision: exponential notation upto 6 digits after the decimal
+while n < 50
     coeffVal = binCoeff(n, n);
     if coeffVal ~= 1
-        fprintf(fp, 'Ineqality at %d, value: %d\n', n, coeffVal);
+        fprintf(fp, 'Value: %d, error: %e\n', n, 1-coeffVal);
     end
     n = n + 1;
 end
+fclose(fp);
