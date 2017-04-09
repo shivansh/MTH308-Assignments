ccd57fb5eebc227426f8763b9e9d6257edaf4b86
Shivansh Rai <shivansh@freebsd.org>
2017-04-09 15:31:25 +0530

    Fix errors

--- MP1-14658/orthoProjectionOnCurve.m
+++ MP1-14658/orthoProjectionOnCurve.m
@@ -1,16 +1,19 @@
 % Orthogonal projection on a curve for a given point
 %
-% Function signature: returns the point on the curve
-% which is closest to the given point (x0, y0).
+% Function signature: returns the parameter t
+%
+% Name: Shivansh Rai
+% Roll: 14658
 
-function orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
+function t0 = orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
     a = 0.001;
     % f(t) = (distance)^2
     dist = @(t) (X(t) - x0)^2 + (Y(t) - y0)^2;
 
-    % Take initial value of t0 and compute the corresponding distance
     % Since the period of the given parametric curve is 1, the relevant
-    % range of values of t is 0.0...0.9
+    % range of values of t is [0,1)
+    % Take initial value of t0 and compute the corresponding distance.
+    % We then find the value of t0 which gives the least distance.
     t0 = 0.0;
     d = dist(0.0);
     for t_iter = 0.1: 0.1: 0.9
@@ -21,20 +24,19 @@ function orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
         end
     end
 
-    % fprintf('d: %i; t0: %i\n', d, t0);
-
     x_prime = @(t) 2*(X(t) - x0)*dXdt(t);
     y_prime = @(t) 2*(Y(t) - y0)*dYdt(t);
     grad_f =  @(t) x_prime(t) + y_prime(t);
 
-    % Stop when difference between consecutive "y" is within eps
-    % We limit the number of iterations to 40
+    % Stop when difference between consecutive "t" is within 0.0001
+    % We limit the number of iterations to be 50 as the initial value
+    % of t was chosen fairly close to the solution, and hence 50
+    % iterations is a
     count = 0;
 
-    while ( a*abs(grad_f(t0)) > eps && count < 40)
+    while ( a*abs(grad_f(t0)) > 0.0001 && count < 50)
         t0 = t0 - a*grad_f(t0);
         count = count + 1;
     end
 
-    fprintf('t: %i\n', t0);
 end
