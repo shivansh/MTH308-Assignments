30a857bb63316d6eb05dacedaee0f03b13973743
Shivansh Rai <shivansh@freebsd.org>
2017-04-09 14:22:46 +0530

    Improve accuracy

--- project/main.m
+++ project/main.m
@@ -1,10 +1,12 @@
 function main()
-    X    = @(t) 2*cos(2*pi*t);
+    X    = @(t) 2*sin(2*pi*t);
     Y    = @(t) 3*sin(2*pi*t);
-    dXdt = @(t) -4*pi*sin(2*pi*t);
+    dXdt = @(t) 4*pi*cos(2*pi*t);
     dYdt = @(t) 6*pi*cos(2*pi*t);
-    x0  = 1;
+    x0  = 2;
     y0  = 7;
     eps = 0.00001;
-    orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps);
+    tic
+    orthoProjectionOnCurve1(x0, y0, X, Y, dXdt, dYdt, eps);
+    toc
 end
--- project/orthoProjectionOnCurve.m
+++ project/orthoProjectionOnCurve.m
@@ -2,9 +2,6 @@
 %
 % Function signature: returns the point on the curve
 % which is closest to the given point (x0, y0).
-%
-% Name: Shivansh Rai
-% Roll: 14658
 
 function orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
     a = 0.001;
@@ -12,10 +9,10 @@ function orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
     dist = @(t) (X(t) - x0)^2 + (Y(t) - y0)^2;
 
     % Take initial value of t0 and compute the corresponding distance
-    % Since the period of the given parametric curve is 1, the safe
+    % Since the period of the given parametric curve is 1, the relevant
     % range of values of t is 0.0...0.9
-    t0 = 0;
-    d = dist(0);
+    t0 = 0.0;
+    d = dist(0.0);
     for t_iter = 0.1: 0.1: 0.9
         temp = dist(t_iter);
         if (temp < d)
@@ -28,13 +25,16 @@ function orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
 
     x_prime = @(t) 2*(X(t) - x0)*dXdt(t);
     y_prime = @(t) 2*(Y(t) - y0)*dYdt(t);
-    x_new = X(t0);
-    y_new = Y(t0);
-    % stop when difference between consecutive "y" is within eps
-    while (a*y_prime(y_new) < eps)
-        x_new = x_new - a*x_prime(x_new);
-        y_new = y_new - a*y_prime(y_new);
+    grad_f =  @(t) x_prime(t) + y_prime(t);
+
+    % Stop when difference between consecutive "y" is within eps
+    % We limit the number of iterations to 40
+    count = 0;
+
+    while ( a*abs(grad_f(t0)) > eps && count < 40)
+        t0 = t0 - a*grad_f(t0);
+        count = count + 1;
     end
 
-    fprintf('x_new: %i; y_new: %i\n', x_new, y_new);
+    fprintf('t: %i\n', t0);
 end
